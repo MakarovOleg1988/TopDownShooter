27b323d8c509231c2cf54448e4e3a1e2ae82be68
79264 <79264@DESKTOP-65729PJ>
2023-06-09 16:16:50 +0300

    - Add Store Data

---
+++ Assets/Scripts/Scene/Items/Store.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace TopDownShooter
+{
+    public class Store : MonoBehaviour
+    {
+        [SerializeField]
+        private StoreItemData _storeItemData;
+
+        [SerializeField]
+        private TextMeshProUGUI[] _nameItem;
+
+        [SerializeField]
+        private TextMeshProUGUI[] _costItem;
+
+        [SerializeField]
+        private TextMeshProUGUI[] _discriptionItem;
+
+        [SerializeField]
+        private Image[] _spriteItem;
+
+        private void Start()
+        {
+
+            SetDataItem();
+        }
+
+        private void SetDataItem()
+        {
+            for (int i = 0; i < _storeItemData.nameItem.Length; i++)
+            {
+                _nameItem[i].text = _storeItemData.nameItem[i];
+                _costItem[i].text = _storeItemData.costItem[i].ToString();
+                _discriptionItem[i].text = _storeItemData.discriptionItem[i];
+                _spriteItem[i].sprite = _storeItemData.spriteItem[i];
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Scene/Items/StoreItemData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace TopDownShooter
+{
+    [CreateAssetMenu(fileName = "Store Item", menuName = "Shop/Create Store Item List")]
+    public class StoreItemData : ScriptableObject
+    {
+        public string[] nameItem;
+        public int[] costItem;
+        public string[] discriptionItem;
+        public Sprite[] spriteItem;
+    }
+}
